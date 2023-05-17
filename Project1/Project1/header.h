ae09e8f69975df45b76717cd49d6bebb0af579da
josipStjepic <132853196+josipStjepic@users.noreply.github.com>
2023-05-17 17:26:21 +0200

    Add files via upload

---
+++ Project1/Project1/header.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef FUNKCIJE_H
+#define FUNKCIJE_H
+
+#define MAX_IGRACA 20
+#define MAX_UTAKMICA 10
+
+typedef struct {
+    char ime[20];
+    char prezime[20];
+    int brojGolova;
+} Igrac;
+
+typedef struct {
+    char protivnik[50];
+    int rezultat;
+} Utakmica;
+
+void unosIgraca(Igrac klub[], int* brojIgraca);
+void unosUtakmice(Utakmica utakmice[], int* brojUtakmica);
+void ispisIgraca(Igrac klub[], int brojIgraca);
+void ispisUtakmica(Utakmica utakmice[], int brojUtakmica);
+void spremanjeIgracaUDatoteku(Igrac klub[], int brojIgraca);
+void spremanjeUtakmicaUDatoteku(Utakmica utakmice[], int brojUtakmica);
+
+#endif
+
