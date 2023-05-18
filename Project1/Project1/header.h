8f15e2970793d3d2c7fe6cabd4a07f927b9bd8c3
josipStjepic <132853196+josipStjepic@users.noreply.github.com>
2023-05-18 12:41:03 +0200

    Update header.h

--- Project1/Project1/header.h
+++ Project1/Project1/header.h
@@ -1,28 +1,24 @@
 #pragma once
-#ifndef FUNKCIJE_H
-#define FUNKCIJE_H
-
-#define MAX_IGRACA 20
-#define MAX_UTAKMICA 10
+#ifndef HEADER_H
+#define HEADER_H
 
 typedef struct {
-    char ime[20];
-    char prezime[20];
-    int brojGolova;
+	char ime[30];
+	int brojGolova;
 } Igrac;
 
 typedef struct {
-    char protivnik[50];
-    int goloviNasaEkipa;
-    int goloviProtivnik;
+	char protivnik[30];
+	int goloviNasaEkipa;
+	int goloviProtivnik;
 } Utakmica;
 
-void unosIgraca(Igrac klub[], int* brojIgraca);
-void unosUtakmice(Utakmica utakmice[], int* brojUtakmica);
-void ispisIgraca(Igrac klub[], int brojIgraca);
-void ispisUtakmica(Utakmica utakmice[], int brojUtakmica);
-void spremanjeIgracaUDatoteku(Igrac klub[], int brojIgraca);
-void spremanjeUtakmicaUDatoteku(Utakmica utakmice[], int brojUtakmica);
-void pretratiRezultate(Utakmica utakmice[],int brojUtakmica);
-#endif
+void unosIgraca(Igrac** klub, int* brojIgraca);
+void ispisIgraca(Igrac* klub, int brojIgraca);
+void unosUtakmice(Utakmica** utakmice, int* brojUtakmica);
+void ispisUtakmica(Utakmica* utakmice, int brojUtakmica);
+void spremanjeIgracaUDatoteku(Igrac* klub, int brojIgraca);
+void spremanjeUtakmicaUDatoteku(Utakmica* utakmice, int brojUtakmica);
+void pretraziRezultate(Utakmica* utakmice, int brojUtakmica);
 
+#endif  // HEADER_H
